598d21ffaf08719c7e0bb16ee331e8c298c43819
ivan <ivansizyh26@gmail.com>
2022-02-18 19:53:00 +0700

    Lesson 4 add answer logic

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,18 @@
+class AnswersController < ApplicationController
+  expose :question, ->{ Question.find(params[:question_id]) }
+  expose :answer, parent: :question
+
+  def create
+    if answer.save
+      redirect_to answer
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:body)
+  end
+end
